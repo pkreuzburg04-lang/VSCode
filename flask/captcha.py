00236072a93c1f1b41df0cd6c48446661dbb395e
Pat Desktop <pkreuzburg04@gmail.com>
2026-03-20 15:31:38 -0400

    trying

--- flask/captcha.py
+++ flask/captcha.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        name = request.form['username']
+        name = request.form.get('next')
         return f"Hello {name}, POST request received"
     return render_template('Captcha text.html')
+
+# --- NEW: endpoint to receive fail messages ---
+@app.route('/captcha text', methods=['POST'])
+def fail():
+    data = request.json
+    print("From frontend:", data)  # printed in terminal
+    return {"status": "received"}
 
 if __name__ == '__main__':
     app.run(debug=True)
